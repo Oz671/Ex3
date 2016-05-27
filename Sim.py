63ebe982a82d09d2c2a3e2a581cf7e57d0486848
liavbak <liavbak@gmail.com>
2016-05-27 19:47:15 +0300

    Update Sim.py

--- Sim.py
+++ Sim.py
@@ -29,10 +29,9 @@ G1=random.randint(100,150)
 w.create_rectangle(G1, G1, G1+50, G1+50, fill='gray')
 G2=random.randint(300,350)
 w.create_rectangle(G2, G2, G2+50, G2+50, fill='gray')
-
-
 robot_sim=[]
-for i in range(0,100):
+number_robots=100
+for i in range(0,number_robots):
     x=random.randint(10,width_height-10)
     y=random.randint(10,width_height-10)
     #CHECK IF BLACK
@@ -54,28 +53,46 @@ for i in range(0,100):
         robot_sim.insert(i, Robot.Robot(x,y,'white',i))
     
 checkered(w,10)
-min=10000
-
 #check robot closest to robotID=50
-for i in range(0,100):
-    if(i!=50):
-        dist=int(math.sqrt(math.pow(robot_sim[i].x-robot_sim[50].x,2)+math.pow(robot_sim[i].y-robot_sim[50].y,2)))
-        if(dist<min):
-            min=dist
-            id=i
-
-print("the robotID best closest is: ",id," dist: ",min)
-
-#move Robots
-for i in range(0,100):
-    robot_sim[i].moveLeft(B1,B2)
-    robot_sim[i].Battery()
+def robotCloset():
+    min=10000
+    for i in range(0,number_robots):
+        if(i!=50):
+            dist=int(math.sqrt(math.pow(robot_sim[i].x-robot_sim[50].x,2)+math.pow(robot_sim[i].y-robot_sim[50].y,2)))
+            if(dist<min):
+                min=dist
+                id=i
     
-for i in range(0,100):
-    w.create_oval(robot_sim[i].x, robot_sim[i].y, robot_sim[i].x+10, robot_sim[i].y+10, fill='blue')
-    w.create_text(robot_sim[i].x, robot_sim[i].y-6, text=i, fill='blue')
+    print("the robotID best closest is: ",id," dist: ",min)
+#move Left Robots
+def move():
+    for i in range(0,number_robots):
+        robot_sim[i].moveLeft(B1,B2)
+        robot_sim[i].Battery()
+    
+    for i in range(0,number_robots):
+        w.create_oval(robot_sim[i].x, robot_sim[i].y, robot_sim[i].x+10, robot_sim[i].y+10, fill='blue')
+        w.create_text(robot_sim[i].x, robot_sim[i].y-6, text=i, fill='blue')
+    
+#sent and accept MSG (r=dist)
+def MSG():
+    r=100
+    for i in range(0,number_robots):
+        count=0
+        while(count<number_robots):
+            if(count!=i and r>int(math.sqrt(math.pow(robot_sim[i].x-robot_sim[count].x,2)+math.pow(robot_sim[i].y-robot_sim[count].y,2)))):
+                robot_sim[i].addMSG(count)
+            count=count+1
+            
+    
+    for i in range(0,number_robots):
+        if(robot_sim[i].msg!=[]):
+            print("RobotID", i,"get msg from", robot_sim[i].msg,"(ID's)")
 
 
+robotCloset()
+move()
+MSG()
 w.mainloop()
 
 
